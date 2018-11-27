f44736de813c76ff220e6c573e4a53592e838e5f
Brian Nguyen <nguyen.brian70@gmail.com>
2018-11-26 19:11:29 -0500

    Updates

---
+++ precache-manifest.9bccd4fda2fde9f7e991ddb7e15bc690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "852e7c56d906e52b6494",
+    "url": "/pitchapp/static/css/main.8a486fac.chunk.css"
+  },
+  {
+    "revision": "852e7c56d906e52b6494",
+    "url": "/pitchapp/static/js/main.852e7c56.chunk.js"
+  },
+  {
+    "revision": "bd6fa8ebcda757b1bfd7",
+    "url": "/pitchapp/static/js/1.bd6fa8eb.chunk.js"
+  },
+  {
+    "revision": "393d27739d057ef31a4e",
+    "url": "/pitchapp/static/js/runtime~main.393d2773.js"
+  },
+  {
+    "revision": "388880bc9a1e2904c568cfdca12a993a",
+    "url": "/pitchapp/index.html"
+  }
+];
